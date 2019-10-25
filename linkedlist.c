6de5d6e1f078998fa08e195061556e0744199d76
Michael Zhang <mzhang00@stuy.edu>
2019-10-25 10:07:40 -0400

    Add all necessary functions

--- linkedlist.c
+++ linkedlist.c
@@ -3,4 +3,28 @@
 #include <string.h>
 #include <time.h>
 
-struct node { int i; struct node *next; };
+struct node {char * name; struct node * next;};
+
+void print_list(struct node *){
+  while (node.next != '\0'){
+    //
+  }
+}
+
+struct node * insert_front(struct node *, int){
+  //
+}
+
+struct node * free_list(struct node *){
+  //
+}
+
+struct node * remove_node(struct node *front, int data){
+  //
+}
+
+int main(){
+    srand(time(NULL));
+
+    return 0;
+}
